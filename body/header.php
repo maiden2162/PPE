a2e99f09b435780c00aa4b6307cc988f9f6fdf9a
maiden2162 <maiden2165@gmail.com>
2017-02-09 22:49:21 +0100

    Add files via upload

--- body/header.php
+++ body/header.php
@@ -1,9 +1,15 @@
 <head>
-  <meta charset="utf-8">
-  <!-CDN bootstrap->
-  <link rel="stylesheet" href="../css/main.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <title>Filelec</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!--Icon-->
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link >  
+<link rel="stylesheet" href="css/main.css">
+
 </head>
--- index.php
+++ index.php
@@ -1,81 +1,58 @@
 <!DOCTYPE html>
 <html>
- <?php include 'body/header.php'; ?>
- <link rel="stylesheet" href="css/main.css">
+    <?php include "body/header.php"?>
   <body>
-    <nav class="navbar navbar-inverse">
-  <div class="container-fluid">
+    <!--navbar1-->
+    <!--navbar pour afficher info du compte quand la personne est co
+    afficher aussi le shop et afficher une barre de recherche-->
+    <nav class="navbar navbar-default" id="jesaispas">
+    <div class="container-fluid">
     <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+        <!--logo de l'entreprise-->
+        <a><img alt="Brand" src="images/logo/b.gif"></a>
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="pages/shops.php">Shops</a></li>
-        <li><a href="pages/contact.php">Contact</a></li>
-        <li class="dropdown">
-    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-      Composants <span class="caret"></span>
-    </a>
-    <ul class="dropdown-menu">
-      <li><a href="#">Carte Mère</a></li>
-      <li><a href="#">Carte graphique</a></li>
-      <li><a href="#">processeur</a></li>
-      <li><a href="#">RAM</a></li>
-      <li><a href="#">Alimentation</a></li>
-      <li><a href="#">Boitier</a></li>
-    </ul>
-  </li>
-
+           <li><a href="#"></a></li>
+           <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
+          <li><a href="#"></a></li>
       </ul>
+      <div class="row">
+        <div class="col-lg-6">
+            <div class="input-group">
+                <span class="input-group-btn">
+                <button class="btn btn-default" type="button">Chercher</button>
+                </span>
+                <input type="text" class="form-control" placeholder="Chercher...">
+            </div><!-- /input-group -->
+        </div><!-- /.col-lg-6 -->
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="pages/connexion.php">Connexion</a></li>
+        <li><a href="#">Link</a></li>
+        <li><a href="#">Link</a></li>
       </ul>
     </div>
   </div>
 </nav>
-<div id="my_carousel" class="carousel slide" data-ride="carousel">
-<!-- Bulles -->
-<ol class="carousel-indicators">
-<li data-target="#my_carousel" data-slide-to="0" class="active"></li>
-<li data-target="#my_carousel" data-slide-to="1"></li>
-<li data-target="#my_carousel" data-slide-to="2"></li>
-</ol>
-<!-- Slides -->
-<div class="carousel-inner">
-<!-- Page 1 -->
-<div class="item active">
-<div class="carousel-page">
-<img src="/images/bootstrap.png" class="img-responsive" style="margin:0px auto;" />
-</div>
-<div class="carousel-caption">Page 1 de présentation</div>
-</div>
-<!-- Page 2 -->
-<div class="item">
-<div class="carousel-page"><img src="/images/twitter-bootstrap.jpg" class="img-responsive img-rounded"
-style="margin:0px auto;"  /></div>
-<div class="carousel-caption">Page 2 de présentation</div>
-</div>
-<!-- Page 3 -->
-<div class="item">
-<div class="carousel-page">
-<img src="/images/tablette.png" class="img-responsive img-rounded"
-style="margin:0px auto;max-height:100%;"  />
-</div>
-<div class="carousel-caption">Page 2 de présentation</div>
-</div>
-</div>
-<!-- Contrôles -->
-<a class="left carousel-control" href="#my_carousel" data-slide="prev">
-<span class="glyphicon glyphicon-chevron-left"></span>
-</a>
-<a class="right carousel-control" href="#my_carousel" data-slide="next">
-<span class="glyphicon glyphicon-chevron-right"></span>
-</a>
-</div>
+  </nav>
+  </body>
 </html>
+
+
+
+
+
+<!--<div class="row">
+  <div class="col-xs-6 col-md-3">
+    <a href="#" class="thumbnail">
+      <img src="..." alt="...">
+    </a>
+  </div>
+  ...
+</div>(pour le shop)-->
